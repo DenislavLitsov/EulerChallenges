82f4fad10624176156c23b5e69601463ad8423fc
Denislav Litsov <denislavlicov@gmail.com>
2023-12-18 00:21:49 +0100

    Challenge 6

---
+++ Challenge6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Challenge6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sumOfSquares = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumOfSquares += i * i;
+            }
+
+
+            long squareOfSum = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                squareOfSum += i;
+            }
+
+            squareOfSum *= squareOfSum;
+
+            Console.WriteLine($"sumOfSquares: {sumOfSquares}");
+            Console.WriteLine($"squareOfSum: {squareOfSum}");
+
+            Console.WriteLine($"Difference: {squareOfSum - sumOfSquares}");
+        }
+    }
+}
