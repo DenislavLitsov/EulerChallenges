a78207c06265c888da503a24f5684be09d1c9c3e
Denislav Litsov <denislavlicov@gmail.com>
2023-12-18 00:56:28 +0100

    Challenge10

---
+++ Challenge10/Program.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+
+namespace Challenge10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<long> primes = new List<long>();
+            for (int i = 2; i < 2000000; i++)
+            {
+                if (i.IsPrime())
+                {
+                    primes.Add(i);
+                    if (primes.Count % 1000 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+            Console.WriteLine(primes.Sum());
+        }
+    }
+}
