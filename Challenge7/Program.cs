5c8828f6788bd619a1ab95eab762bfd89a04bf0d
Denislav Litsov <denislavlicov@gmail.com>
2023-12-18 00:31:43 +0100

    Challenge 7

---
+++ Challenge7/Program.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+
+namespace Challenge7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> primes = new List<long>();
+            for (long i = 3; i < long.MaxValue; i += 2)
+            {
+                if (i.IsPrime())
+                {
+                    primes.Add(i);
+
+                    Console.WriteLine($"New prime: {i}, TotalSize: {primes.Count}");
+                }
+
+                if (primes.Count == 10001)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Result: ");
+            Console.WriteLine(primes.Last());
+        }
+    }
+}
--- Common/AdvancedMath.cs
+++ Common/AdvancedMath.cs
@@ -2,6 +2,40 @@
 {
     public static class AdvancedMath
     {
+        public static bool IsPrime(this long number)
+        {
+            if (number == 2)
+            {
+                return true;
+            }
 
+            for (var i = 2; i < number / 2 + 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsPrime(this int number)
+        {
+            if (number == 2)
+            {
+                return true;
+            }
+
+            for (var i = 2; i < number / 2 + 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
