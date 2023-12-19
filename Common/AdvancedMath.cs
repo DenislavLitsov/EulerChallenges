1c7a07778f31876fb4bb00d7da9d6f6eac9eeffd
Denislav Litsov <denislavlicov@gmail.com>
2023-12-19 18:01:44 +0100

    5 Times Faster prime check algorithm

--- Common/AdvancedMath.cs
+++ Common/AdvancedMath.cs
@@ -4,36 +4,30 @@
     {
         public static bool IsPrime(this long number)
         {
-            if (number == 2)
-            {
-                return true;
-            }
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
 
-            for (var i = 2; i < number / 2 + 1; i++)
-            {
+            var boundary = (int)(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
                 if (number % i == 0)
-                {
                     return false;
-                }
-            }
 
             return true;
         }
 
         public static bool IsPrime(this int number)
         {
-            if (number == 2)
-            {
-                return true;
-            }
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
 
-            for (var i = 2; i < number / 2 + 1; i++)
-            {
+            var boundary = (int)(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
                 if (number % i == 0)
-                {
                     return false;
-                }
-            }
 
             return true;
         }
