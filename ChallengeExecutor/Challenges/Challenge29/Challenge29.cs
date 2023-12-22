3fe561e77a83a99a06c35a3e0e5cc245009623c0
Denislav Litsov <denislavlicov@gmail.com>
2023-12-22 21:11:34 +0100

    Challenge29

---
+++ ChallengeExecutor/Challenges/Challenge29/Challenge29.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using System.Numerics;
+
+namespace ChallengeExecutor.Challenges.Challenge29
+{
+    public class Challenge29 : BaseChallenge<int>
+    {
+        public override string GetName()
+        {
+            return "Challenge29";
+        }
+
+        protected override int SolveImplementation()
+        {
+            List<BigInteger> sequence = new List<BigInteger>();
+
+            for (BigInteger a = 2; a <= 100; a++)
+            {
+                for (int b = 2; b <= 100; b++)
+                {
+                    BigInteger c = a.BigPower(b);
+                    if (sequence.Contains(c) == false)
+                    {
+                        sequence.Add(c);
+                    }
+                }
+            }
+
+            return sequence.Count;
+        }
+    }
+}
