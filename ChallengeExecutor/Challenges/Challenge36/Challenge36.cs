339ab547bf43d06a2f2fb7a058fea94f8bfe18de
Denislav Litsov <denislavlicov@gmail.com>
2023-12-23 22:30:14 +0100

    Challenge36

---
+++ ChallengeExecutor/Challenges/Challenge36/Challenge36.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+
+namespace ChallengeExecutor.Challenges.Challenge36
+{
+    public class Challenge36 : BaseChallenge<int>
+    {
+        public override string GetName()
+        {
+            return "Challenge36";
+        }
+
+        protected override int SolveImplementation()
+        {
+            int totalSum = 0;
+
+            for (int i = 1; i < 1000000; i++)
+            {
+                string mainNumber = i.ToString();
+                string toCheck = new string(mainNumber.Reverse().ToArray());
+                if (mainNumber != toCheck)
+                    continue;
+
+                string zeroNumber = Convert.ToString(i, 2);
+                toCheck = new string(zeroNumber.Reverse().ToArray());
+                if (zeroNumber.Last() == '0')
+                    continue;
+
+                if (zeroNumber != toCheck)
+                    continue;
+
+                totalSum += i;
+            }
+
+            return totalSum;
+        }
+    }
+}
